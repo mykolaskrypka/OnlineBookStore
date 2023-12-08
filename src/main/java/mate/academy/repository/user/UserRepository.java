f9595b496098de1d67d530391c537d272ebe0dcf
Mykola Skrypka <mykolaskrypka@gmail.com>
2023-12-08 21:39:32 +0200

    added default users creation

--- src/main/java/mate/academy/repository/user/UserRepository.java
+++ src/main/java/mate/academy/repository/user/UserRepository.java
@@ -5,8 +5,5 @@ import mate.academy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Override
-    User save(User user);
-
     Optional<User> findByEmail(String email);
 }
