62b866cd2de90382a36c0d3d915797991b7e20ab
Mykola Skrypka <mykolaskrypka@gmail.com>
2023-12-12 23:40:16 +0200

    added EmailValidator

--- src/main/java/mate/academy/dto/UserRegistrationRequestDto.java
+++ src/main/java/mate/academy/dto/UserRegistrationRequestDto.java
@@ -4,12 +4,14 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import mate.academy.validation.FieldMatch;
+import mate.academy.validation.ValidEmail;
 
 @Data
 @FieldMatch(first = "password", second = "repeatPassword",
         message = "The password fields must match")
 public class UserRegistrationRequestDto {
     @NotNull
+    @ValidEmail
     private String email;
     @NotNull
     @Size(min = 6, max = 20)
