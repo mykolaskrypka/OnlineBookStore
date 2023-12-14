7afd23a453c56010edae414a4db5894d7a8da1cb
Mykola Skrypka <mykolaskrypka@gmail.com>
2023-12-14 22:08:37 +0200

    deleted custom @ValidEmail

--- src/main/java/mate/academy/dto/UserRegistrationRequestDto.java
+++ src/main/java/mate/academy/dto/UserRegistrationRequestDto.java
@@ -1,17 +1,17 @@
 package mate.academy.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import mate.academy.validation.FieldMatch;
-import mate.academy.validation.ValidEmail;
 
 @Data
 @FieldMatch(first = "password", second = "repeatPassword",
         message = "The password fields must match")
 public class UserRegistrationRequestDto {
     @NotNull
-    @ValidEmail
+    @Email
     private String email;
     @NotNull
     @Size(min = 6, max = 20)
